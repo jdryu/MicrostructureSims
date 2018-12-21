7a7081eba2247454e08c40f2b6b720253295a92f
James Yu <jd.ryu@icloud.com>
2018-12-20 19:40:03 -0500

    Update Fine-Grained 2D.py

--- Fine-Grained 2D.py
+++ Fine-Grained 2D.py
@@ -12,46 +12,49 @@ from Constants import *
 
 start = time.time()
 
-cvs_res = 500
+cvs_res = 1000
 
 x = np.arange(0, cvs_res, 1)
 y = np.arange(0, cvs_res, 1)
-Xg, Yg = np.meshgrid(x, y, sparse=True)
+Xg, Yg = np.meshgrid(x, y, sparse=False)
 Vg = np.zeros((cvs_res, cvs_res))
 
-r_mean_px = calibrate(r_mean, 40, 500)
-r_std_px = calibrate(r_std, 40, 500)
+r_mean_px = calibrate(r_mean, 40, cvs_res)
+r_std_px = calibrate(r_std, 40, cvs_res)
 n = int((area_frac * cvs_res**2) / (math.pi * r_mean_px**2))
 print('Total number of grains:', n)
 m = 0
 
-exc = [[0, 0]]
+# defines allowed nucleation sites for grains, randomly picks the center of each circular grain from these
+site_x = np.arange(r_mean_px//2, cvs_res-r_mean_px//2, int(2*r_mean_px))
+site_y = np.arange(r_mean_px//2, cvs_res-r_mean_px//2, int(2*r_mean_px))
+lng = len(site_x) - 1
+Xs, Ys = np.meshgrid(site_x, site_y, sparse=False)
+np.random.shuffle(Xs.flat)
+np.random.shuffle(Ys.flat)
+
 for i in range(n):
     r = rand.gauss(r_mean_px, r_std_px)
     c = 2*r/2.35482
 
-    while True:
-        nuc_x = rand.randint(0, cvs_res)
-        nuc_y = rand.randint(0, cvs_res)
-        min_dist = cvs_res * 2
-        for pt in exc:
-            dist = linalg.norm(np.array([nuc_x, nuc_y]) - np.array(pt))
-            if dist < min_dist:
-                min_dist = dist
+    # allows fluctuations around selected nucleation site to recover some randomness
+    nuc_x = Xs[i%lng, i//lng] + (rand.randint(0, r_mean_px)-r_mean_px//2)
+    nuc_y = Ys[i%lng, i//lng] + (rand.randint(0, r_mean_px)-r_mean_px//2)
 
-        if min_dist >= 2*r_mean_px:
-            m += 1
-            print('Drawing grain #' + str(m) + ' at point: (' + str(nuc_x) + ', ' + str(nuc_y) + ')')
-            exc.append([nuc_x, nuc_y])
-            break
+    m += 1
+    print('Drawing grain #' + str(m) + ' at point: (' + str(nuc_x) + ', ' + str(nuc_y) + ')')
 
-    Vg = Vg + np.exp(-((Xg-nuc_x)**2 + (Yg-nuc_y)**2)/(2*c)**2)
+    V = np.exp(-(((Xg-nuc_x)**2)/(2*(c**2)) + ((Yg-nuc_y)**2)/(2*(c**2))))
+    V_thresh = np.where(V >= 0.5, 1, 0)
+    Vg += V_thresh
 
 Z = np.where(Vg >= 0.5, 1, 0)
+a_frac = np.sum(Z) / cvs_res**2
+print('Area fraction:', a_frac)
 
 end = time.time()
-comp_time = start - end
-print('Total computation time:', comp_time)
+comp_time = end - start
+print('Total computation time (in seconds):', comp_time)
 
 plt.matshow(Z, cmap="gray")
 plt.show()
